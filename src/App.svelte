c67f483b42045e97b9a60ff4a926fd52a857cbd8
schmar00 <martin@schiegl.eu>
2023-12-09 15:55:56 +0100

    #

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,21 @@
 <script>
   import Counter from './lib/Counter.svelte'
+  import Navbar from './lib/navbar.svelte';
+  import Card from './lib/card.svelte';
 </script>
 
+
 <main>
+  <Navbar />
+
   <div class="card">
     <Counter />
   </div>
+  
+  <Card />
 
 </main>
 
 <style>
- 
+
 </style>
--- src/lib/Counter.svelte
+++ src/lib/Counter.svelte
@@ -4,7 +4,7 @@
     count += 1
   }
 </script>
-
-<button on:click={increment}>
+<button on:click={increment}
+  class="focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:focus:ring-yellow-900">
   count is {count}
 </button>
