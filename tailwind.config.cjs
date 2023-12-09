a92aa12e79968b2a3f8037de05f0b74e8ad8b557
schmar00 <martin@schiegl.eu>
2023-12-09 15:32:44 +0100

    #

---
+++ tailwind.config.cjs
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config}*/
+const config = {
+  content: ["./src/**/*.{html,js,svelte,ts}",
+  "./node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}",],
+  
+
+  theme: {
+    extend: {},
+  },
+
+  plugins: [
+    require('flowbite/plugin')
+  ],
+  darkMode: 'media',
+};
+
+module.exports = config;
